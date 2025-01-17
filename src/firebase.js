52d642176ddd9a6d443ebe0292c658880fc4cdc3
paveldsh1 <pdsh001@gmail.com>
2025-01-17 15:15:22 +0400

    Added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import { initializeApp } from '@firebase/app';
+import { getFirestore } from '@firebase/firestore';
+import { getAuth } from '@firebase/auth';
+import { getStorage } from '@firebase/storage';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDOJ5fNAHsbye9cSIRk0BwVru3z9geTqnk",
     authDomain: "linkedin-clone-yt-50084.firebaseapp.com",
@@ -5,4 +10,11 @@ const firebaseConfig = {
     storageBucket: "linkedin-clone-yt-50084.firebasestorage.app",
     messagingSenderId: "376353743947",
     appId: "1:376353743947:web:d4e9ed412d774a6aa8d9ce"
-  };
+};  
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+export { db, auth, storage };
