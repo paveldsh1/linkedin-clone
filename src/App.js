758f98d9fd38273a2cbda2eb51ef8cb8d337094f
paveldsh1 <pdsh001@gmail.com>
2025-01-20 15:05:51 +0400

    refactoring userRecuser

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import Header from './Header';
 import './Header.css';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header></Header>
-      <div className="app__body">
-        <Sidebar></Sidebar>
-        <Feed></Feed>
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar></Sidebar>
+          <Feed></Feed>
+        </div>
+      )}
     </div>
   );
 }
